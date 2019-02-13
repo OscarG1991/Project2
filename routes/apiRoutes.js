8243dd1a8bf0dd1425841f2f1445db756b5da83b
Carlo <gianqui.f@gmail.com>
2019-02-12 20:27:59 -0700

    add get and post routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,31 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+  // this route should find all superheroes in the table and display them as JSON
+  app.get("/api/characters", function(req, res) {
+    db.superHero
+      .findAll()
+      .then(function(characters) {
+        res.json(characters);
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.json(err);
+      });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // this route should add a new character to the table
+  app.post("/api/characters", function(req, res) {
+    db.superHero.create(req.body).then(function(characters) {
+      res.json(characters);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(characters) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
