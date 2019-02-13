cc52f4e8e9f15582d6ab3ca5141827739e689554
Carlo <gianqui.f@gmail.com>
2019-02-12 21:22:43 -0700

    add schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS superheroesdb;
+CREATE DATABASE superheroesdb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
